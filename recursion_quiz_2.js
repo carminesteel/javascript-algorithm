8e0b7f7dc032ef07b74c042ff6ffe6246d9e3cda
carminesteel <90226606+PMStudios-KHC@users.noreply.github.com>
2024-06-19 16:51:56 +0900

    recursion quiz 2

---
+++ recursion_quiz_2.js
@@ -0,0 +1,18 @@
+// factorial
+// 숫자를 받아 해당 숫자의 계승(팩토리얼)을 반환하는 팩토리얼 함수를 작성하시오.
+
+// 팩토리얼은 정수와 그 아래의 모든 정수의 곱입니다.
+
+// 예를 들어, 4 팩토리얼 (4!)은 4 * 3 * 2 * 1 = 2입니다. 팩토리얼 0(0!)은 항상 1입니다.
+
+factorial(1); // 1
+factorial(2); // 2
+factorial(4); // 24
+console.log(factorial(7)); // 5040
+
+function factorial(val) {
+  if (val === 0) return 1;
+  if (val === 1) return 1;
+  let newVal = val - 1;
+  return val * factorial(newVal);
+}
